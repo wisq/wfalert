ab896c563c1c59646842e280bf6c5436912ee18a
Adrian Irving-Beer <wisq@wisq.net>
2018-09-17 05:39:58 -0400

    Add wfalert.alerts task.

---
+++ lib/mix/tasks/wfalert.alerts.ex
@@ -0,0 +1,61 @@
+defmodule Mix.Tasks.Wfalert.Alerts do
+  use Mix.Task
+  alias WFAlert.{WorldState, Alert, Reward}
+
+  @shortdoc "Show current alerts, and optionally see if they match"
+
+  def run([]) do
+    Mix.Task.run("app.start")
+
+    WorldState.alerts()
+    |> Enum.map(&show_alert/1)
+    |> IO.puts()
+  end
+
+  def run([file]) do
+    Mix.Task.run("app.start")
+
+    Code.eval_file(file)
+
+    WorldState.alerts()
+    |> Enum.map(&show_alert_and_match/1)
+    |> IO.puts()
+  end
+
+  def run(_) do
+    Mix.raise("Usage: mix wfalert.alerts [config file]")
+  end
+
+  defp show_alert(alert) do
+    [
+      "\n--- Alert: #{alert.id} ---\n",
+      alert.rewards
+      |> Enum.sort_by(& &1.name)
+      |> Enum.map(&show_reward/1)
+    ]
+  end
+
+  defp show_alert_and_match(alert) do
+    [
+      show_alert(alert),
+      if Alert.match?(alert) do
+        "- Matches alert filters.\n"
+      else
+        "- Does NOT match alert filters.\n"
+      end
+    ]
+  end
+
+  defp show_reward(reward) do
+    desc = Reward.describe(reward)
+
+    [
+      "- #{inspect(desc)} (#{inspect(reward.category)})\n",
+      if is_atom(reward.id) do
+        ""
+      else
+        "  id: #{inspect(reward.id)}\n"
+      end
+    ]
+  end
+end
