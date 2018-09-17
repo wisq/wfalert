4c5f5de2fee4f38d21d066b09da26108a66f7d0f
Adrian Irving-Beer <wisq@wisq.net>
2018-09-17 03:29:18 -0400

    Add example pushover config.

---
+++ config/pushover.example.exs
@@ -0,0 +1,6 @@
+use Mix.Config
+
+# Copy this to config/pushover.exs and put in your real keys.
+config :wfalert,
+  pushover_api_token: "your token here",
+  pushover_user_key: "your user key here"
