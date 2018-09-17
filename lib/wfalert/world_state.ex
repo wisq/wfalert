ea7a5964154cfd0e7b5e0a111a98207f673e963e
Adrian Irving-Beer <wisq@wisq.net>
2018-09-17 02:05:16 -0400

    Add new_alerts, new_invasions.

--- lib/wfalert/world_state.ex
+++ lib/wfalert/world_state.ex
@@ -1,6 +1,5 @@
 defmodule WFAlert.WorldState do
-  alias WFAlert.Alert
-  alias WFAlert.Invasion
+  alias WFAlert.{Alert, Invasion, Seen}
 
   @uri "http://content.warframe.com/dynamic/worldState.php"
 
@@ -14,6 +13,22 @@ defmodule WFAlert.WorldState do
     state
     |> Map.fetch!("Alerts")
     |> Enum.map(&Alert.parse/1)
+  end
+
+  def new_alerts(state \\ fetch()) do
+    seen = Seen.alerts()
+    alerts = alerts(state)
+
+    Seen.update_alerts(alerts)
+    Enum.reject(alerts, &(&1.id in seen))
+  end
+
+  def new_invasions(state \\ fetch()) do
+    seen = Seen.invasions()
+    invs = invasions(state)
+
+    Seen.update_invasions(invs)
+    Enum.reject(invs, &(&1.id in seen))
   end
 
   def invasions(state \\ fetch()) do
